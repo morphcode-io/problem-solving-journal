1b4c855b4eb6d9a695a0e8226d2c7d60771be54f
Anibal Laura <morphcode.io@gmail.com>
2026-08-25 12:44:13 -0400

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = [num for num in nums]
+        
+        for num in nums:
+            ans.append(num)
+        
+        return ans
